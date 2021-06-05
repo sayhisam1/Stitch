4af4e2e20098671eb7bb06313c08c34007375a90
sam <sayhisam1@gmail.com>
2021-06-05 19:09:44 -0400

    HashMappedTrie: Clean up implementation

--- src/Shared/HashMappedTrie.lua
+++ src/Shared/HashMappedTrie.lua
@@ -1,83 +1,124 @@
-local MAX_BUCKET_SIZE = 32
-local HashMappedTrie = {}
-
 local _COUNT = {}
 local _KEYS = {}
+local DEFAULT_BUCKET_SIZE = 8
+local HashMappedTrie = {}
+HashMappedTrie.__index = HashMappedTrie
 
 local function shallowCopy(tbl)
-	local newtbl = {}
+	local newtbl = table.move(tbl, 1, #tbl, 1, {})
+	debug.profilebegin("HashMappedTrie:shallowCopy")
 	for k, v in pairs(tbl) do
 		newtbl[k] = v
 	end
+	debug.profileend()
 	return newtbl
 end
 
-function HashMappedTrie.get(tbl: table, key: string)
-	if not tbl then
-		error("table was nil!", 2)
-	end
-	local level = 1
-	local hash = string.sub(key, level, level)
-	while tbl ~= nil and ((tbl[_KEYS] and tbl[_KEYS][key] == nil) or tbl[hash] ~= nil) do
-		tbl = tbl[hash]
-		level += 1
-		hash = string.sub(key, level, level)
-	end
-	return (tbl and tbl[_KEYS] and tbl[_KEYS][key]) or nil
+function HashMappedTrie.new(bucketSize: int)
+	local self = setmetatable({
+		bucketSize = bucketSize or DEFAULT_BUCKET_SIZE,
+		data = {
+			[_COUNT] = 0,
+		},
+	}, HashMappedTrie)
+	return self
 end
 
-local function insert(tbl: table, key: string, value: any, level: int)
-	tbl = shallowCopy(tbl)
+function HashMappedTrie:getHash(key: string, depth: int)
+	return (string.byte(key, depth) % self.bucketSize) + 1
+end
 
-	-- key already exists; we just update
-	-- TODO: shrink trie when removing keys
-	if tbl[_KEYS] and tbl[_KEYS][key] ~= nil then
-		tbl[_KEYS] = shallowCopy(tbl[_KEYS])
-		tbl[_KEYS][key] = value
+function HashMappedTrie:get(key: string)
+	debug.profilebegin("HashMappedTrie:get")
+	local depth = 1
+	local hash = HashMappedTrie.getHash(self, key, depth)
+	local data = self.data
+	while not data[key] and data[hash] do
+		data = data[hash]
+		depth += 1
+		hash = HashMappedTrie.getHash(self, key, depth)
+	end
+	local returnValue = data[key]
+	debug.profileend()
+	return returnValue
+end
+
+function HashMappedTrie:set(key: string, value: any)
+	debug.profilebegin("HashMappedTrie:set")
+	local newMap = HashMappedTrie.new(self.bucketSize)
+	newMap.data = shallowCopy(self.data)
+
+	local data = newMap.data
+	local depth = 1
+	local hash = HashMappedTrie.getHash(self, key, depth)
+
+	while not data[key] and data[hash] do
+		data[hash] = shallowCopy(data[hash])
+		data = data[hash]
+		depth += 1
+		hash = HashMappedTrie.getHash(self, key, depth)
+	end
+
+	-- key already exists - just need to update value
+	if data[key] then
+		data[key] = value
 		if value == nil then
-			tbl[_KEYS][_COUNT] -= 1
+			data[_COUNT] -= 1
 		end
-		return tbl
+		debug.profileend()
+		return newMap
 	end
 
-	local hash = string.sub(key, level, level)
-	if hash ~= "" and tbl[hash] then
-		tbl[hash] = insert(tbl[hash], key, value, level + 1)
-		return tbl
-	end
-
-	tbl[_KEYS] = tbl[_KEYS] or {}
-	tbl[_KEYS][key] = value
-	tbl[_KEYS][_COUNT] = (tbl[_KEYS][_COUNT] and tbl[_KEYS][_COUNT] + 1) or 1
-
-	-- if we didn't do this, then the trie would never be rebalanced!
-	if tbl[_KEYS][_COUNT] > MAX_BUCKET_SIZE then
-		-- rebalance current level
-		-- TODO: speed up rebalance by doing this mutably
-		local newTbl = shallowCopy(tbl)
-		newTbl[_KEYS] = nil
-		for k, v in pairs(tbl[_KEYS]) do
+	-- must rebalance in this case
+	if data[_COUNT] + 1 > newMap.bucketSize then
+		local newData = table.create(newMap.bucketSize)
+		for i = 1, newMap.bucketSize do
+			newData[i] = {
+				[_COUNT] = 0,
+			}
+		end
+		for k, v in pairs(data) do
 			if k ~= _COUNT then
-				local k_hash = string.sub(k, level, level)
-				-- must consider case where key is too short (failsafe!)
-				if k_hash == "" then
-					newTbl = insert(newTbl, k, v, level)
-				else
-					newTbl[k_hash] = insert(newTbl[k_hash] or {}, k, v, level + 1)
-				end
+				local k_hash = HashMappedTrie.getHash(newMap, k, depth)
+				newData[k_hash][k] = v
+				newData[k_hash][_COUNT] += 1
 			end
 		end
-		return newTbl
+		for k, _ in pairs(data) do
+			data[k] = nil
+		end
+		for k, v in pairs(newData) do
+			data[k] = v
+		end
+		data = data[hash]
 	end
 
-	return tbl
+	data[key] = value
+	data[_COUNT] += 1
+
+	debug.profileend()
+	return newMap
 end
 
-function HashMappedTrie.set(tbl: table, key: string, value: any)
-	if not tbl then
-		error("table was nil!", 2)
+function HashMappedTrie:getAllKeyValues(map: table)
+	map = map or {}
+
+	local function populate(data)
+		if data[_COUNT] ~= nil then
+			for k, v in pairs(data) do
+				if k ~= _COUNT then
+					map[k] = v
+				end
+			end
+		else
+			for i = 1, self.bucketSize do
+				populate(data[i])
+			end
+		end
 	end
-	return insert(tbl, key, value, 1)
+
+	populate(self.data)
+	return map
 end
 
 return HashMappedTrie
--- src/Shared/HashMappedTrie.spec.lua
+++ src/Shared/HashMappedTrie.spec.lua
@@ -4,13 +4,13 @@ local HashMappedTrie = require(script.Parent.HashMappedTrie)
 return function()
 	describe("insertion", function()
 		it("should insert a single value", function()
-			local trie = {}
+			local trie = HashMappedTrie.new()
 			local val = {}
 			trie = HashMappedTrie.set(trie, "TESTINGABCD", val)
 			expect(HashMappedTrie.get(trie, "TESTINGABCD")).to.equal(val)
 		end)
 		it("should insert multiple values", function()
-			local trie = {}
+			local trie = HashMappedTrie.new()
 			local values = {
 				AAAAAAA = {},
 				BBBBBBB = {},
@@ -26,7 +26,7 @@ return function()
 			end
 		end)
 		it("should resize hashmap", function()
-			local trie = {}
+			local trie = HashMappedTrie.new()
 			local values = {}
 			for i = 1, 1024, 1 do
 				local key = HttpService:GenerateGUID(false)
@@ -37,6 +37,41 @@ return function()
 			end
 			for k, v in pairs(values) do
 				expect(HashMappedTrie.get(trie, k)).to.equal(v)
+			end
+		end)
+		it("should get all key-value pairs", function()
+			local trie = HashMappedTrie.new()
+			local values = {}
+			for i = 1, 128, 1 do
+				local key = HttpService:GenerateGUID(false)
+				values[key] = {}
+			end
+			for k, v in pairs(values) do
+				trie = HashMappedTrie.set(trie, k, v)
+			end
+			for k, v in pairs(HashMappedTrie.getAllKeyValues(trie)) do
+				expect(values[k]).to.equal(v)
+				values[k] = nil
+			end
+			for k, v in pairs(values) do
+				expect(false).to.equal(true)
+			end
+		end)
+		it("should be immutable", function()
+			local trie = HashMappedTrie.new()
+			local values = {}
+			for i = 1, 1024, 1 do
+				local key = HttpService:GenerateGUID(false)
+				values[key] = {}
+			end
+			local newtrie = trie
+			for k, v in pairs(values) do
+				local tmp = HashMappedTrie.set(trie, k, v)
+				expect(HashMappedTrie.get(newtrie, k)).to.never.be.ok()
+				newtrie = tmp
+			end
+			for k, v in pairs(values) do
+				expect(HashMappedTrie.get(trie, k)).to.never.be.ok()
 			end
 		end)
 	end)
