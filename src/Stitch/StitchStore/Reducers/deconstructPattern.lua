65c39d788a3955f27967c18acbdf2730577a7ec7
sam <sayhisam1@gmail.com>
2021-06-19 21:16:05 -0400

    actions/deconstruct: remove stack trace from errors

--- src/Stitch/StitchStore/Reducers/deconstructPattern.lua
+++ src/Stitch/StitchStore/Reducers/deconstructPattern.lua
@@ -10,18 +10,21 @@ return function(stitch)
 		local pattern_state = state["data"][uuid]
 		if not pattern_state then
 			debug.profileend()
-			error(("tried to decontruct non-existent pattern with uuid %s!"):format(uuid))
+			error(("tried to decontruct non-existent pattern with uuid %s!"):format(uuid), 0)
 		end
 
 		for patternName, attachedUuid in pairs(pattern_state["attached"]) do
 			-- skip root patterns
 			if attachedUuid ~= uuid then
 				debug.profileend()
-				error(("tried to deconstruct %s, but has attached child %s of pattern %s!"):format(
-					uuid,
-					attachedUuid,
-					patternName
-				))
+				error(
+					("tried to deconstruct %s, but has attached child %s of pattern %s!"):format(
+						uuid,
+						attachedUuid,
+						patternName
+					),
+					0
+				)
 			end
 		end
 
@@ -32,7 +35,7 @@ return function(stitch)
 		local ref_state = state["data"][refuuid]
 		if not ref_state then
 			debug.profileend()
-			error(("tried to deconstruct %s, but could not reference parent %s!"):format(uuid, refuuid))
+			error(("tried to deconstruct %s, but could not reference parent %s!"):format(uuid, refuuid), 0)
 		end
 
 		state = Util.shallowCopyOnce(state, copied)
