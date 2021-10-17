df88eda660044a14123742d4a178d20d5ef04444
sayhisam1 <sayhisam1@gmail.com>
2021-10-17 02:48:35 -0400

    treewide: Remove observers

--- src/World/System.lua
+++ src/World/System.lua
@@ -1,5 +1,4 @@
 local RunService = game:GetService("RunService")
-local Observer = require(script.Parent.Observer)
 local EntityQuery = require(script.Parent.EntityQuery)
 local inlinedError = require(script.Parent.Parent.Shared.inlinedError)
 
@@ -8,16 +7,6 @@ System.__index = System
 
 System.priority = 1000
 System.updateEvent = RunService.Heartbeat
-
-function System:createObserver(...)
-	local observer = Observer.new(...)
-	if not self._observers then
-		self._observers = {}
-	end
-
-	table.insert(self._observers, observer)
-	return observer
-end
 
 function System:createQuery()
 	return EntityQuery.new(self.stitch.entityManager)
@@ -32,58 +21,16 @@ function System:registerComponent(component)
 	table.insert(self._components, component)
 end
 
-function System:connectEntityAddedSignal(componentResolvable: string | {}, callback: ({}) -> nil)
-	if not self._listeners then
-		self._listeners = {}
-	end
-
-	local listener = self.stitch.entityManager:getEntityAddedSignal(componentResolvable):connect(callback)
-	table.insert(self._listeners, listener)
-end
-
-function System:connectEntityChangedSignal(componentResolvable: string | {}, callback: ({}) -> nil)
-	if not self._listeners then
-		self._listeners = {}
-	end
-
-	local listener = self.stitch.entityManager:getEntityChangedSignal(componentResolvable):connect(callback)
-	table.insert(self._listeners, listener)
-end
-
-function System:connectEntityRemovedSignal(componentResolvable: string | {}, callback: ({}) -> nil)
-	if not self._listeners then
-		self._listeners = {}
-	end
-
-	local listener = self.stitch.entityManager:getEntityRemovedSignal(componentResolvable):connect(callback)
-	table.insert(self._listeners, listener)
-end
-
 function System:create()
 	xpcall(self.onCreate, inlinedError, self, self.stitch)
 end
 
 function System:update()
 	xpcall(self.onUpdate, inlinedError, self, self.stitch)
-	if self._observers then
-		for _, observer in ipairs(self._observers) do
-			observer:clear()
-		end
-	end
 end
 
 function System:destroy()
 	xpcall(self.onDestroy, inlinedError, self, self.stitch)
-	if self._observers then
-		for _, observer in ipairs(self._observers) do
-			observer:destroy()
-		end
-	end
-	if self._listeners then
-		for _, listener in ipairs(self._listeners) do
-			listener:disconnect()
-		end
-	end
 	if self._components then
 		for _, component in ipairs(self._components) do
 			self.stitch.entityManager.collection:unregister(component)
