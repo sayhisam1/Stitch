6fda8c2d530c84a7ceb4a10387b29e675d20534f
sayhisam1 <sayhisam1@gmail.com>
2021-10-19 03:50:25 -0400

    moonwave: add docs

--- src/World/ComponentDefinition.lua
+++ src/World/ComponentDefinition.lua
@@ -1,10 +1,76 @@
 --!strict
 local Util = require(script.Parent.Parent.Shared.Util)
+export type ComponentDefinition = {
+	name: string,
+	defaults: {}?,
+	validators: {}?,
+	destructor: (any, any) -> nil,
+} | ModuleScript
 
+--[=[
+	@type ComponentResolvable ComponentDefinition | string
+	@within ComponentDefinition
+	Whenever an API refers to a `ComponentResolvable`,
+	either the definition or the name of the component can be used.
+]=]
+
+--[=[
+	@class ComponentDefinition
+
+	A ComponentDefinition is a table that defines a Component.
+
+	All ComponentDefinitions must be registered to a World before they can be used.
+]=]
 local ComponentDefinition = {}
 ComponentDefinition.__index = ComponentDefinition
+
+--[=[
+	@prop name string?
+	@within ComponentDefinition
+	Used to identify the Component in the World (should be unique).
+
+	If `name` is not defined and the ComponentDefinition is returned by a ModuleScript,
+	then registering the ModuleScript will use the name of the ModuleScript.
+]=]
+ComponentDefinition.name = nil
+
+--[=[
+	@prop defaults {}?
+	@within ComponentDefinition
+	The default data to use when adding a component. If data is provided to `World.addComponent`,
+	then the data will be merged with the defaults.
+]=]
 ComponentDefinition.defaults = {}
+
+--[=[
+	@prop validators {string : (any) -> boolean}?
+	@within ComponentDefinition
+	A table of functions that will be called to validate the data when adding or changing a component.
+	
+	The value of each key is a function that accepts data[key] and returns true if the value is valid.
+]=]
 ComponentDefinition.validators = {}
+
+--[=[
+	@prop tag boolean? | string?
+	@within ComponentDefinition
+	Used to apply CollectionService tags to the entity the component is attached to
+	(see: `TagSystem` for more details).
+
+	If tag is `true`, the tag will be the same as the name of the ComponentDefinition.
+
+	**Note: To use this property, add the `TagSystem` to the World.**
+]=]
+ComponentDefinition.tag = nil
+
+--[=[
+	@prop replicate boolean?
+	@within ComponentDefinition
+	Enables replication of the component (see: `ReplicationSystem` for more details).
+
+	**Note: To use this property, add the `ReplicationSystem` to the World.**
+]=]
+ComponentDefinition.tag = nil
 
 function ComponentDefinition:createFromData(data: {}?): {}
 	data = Util.mergeTable(Util.deepCopy(self.defaults), data or {})
--- src/World/init.lua
+++ src/World/init.lua
@@ -5,9 +5,22 @@ local EntityManager = require(script.EntityManager)
 local SystemManager = require(script.SystemManager)
 local EntityQuery = require(script.EntityQuery)
 
+--[=[
+	@class World
+
+	The World class is a collection of entities, components, and systems.
+	It is the central class that ties all other parts of an ECS together.
+	A game could have many worlds, but normally there is only one world per game.
+]=]
 local World = {}
 World.__index = World
 
+--[=[
+	Creates a new World.
+
+	@param namespace string? -- The namespace to use for this world.
+	@return World
+]=]
 function World.new(namespace: string)
 	namespace = namespace or DEFAULT_NAMESPACE
 
@@ -23,6 +36,12 @@ function World.new(namespace: string)
 	return self
 end
 
+--[=[
+	Destroys a World. 
+	Components are detached from entities and Systems are stopped.
+
+	@return nil
+]=]
 function World:destroy()
 	-- explicitly unregister all entities first to ensure system state components are properly cleaned up
 	for entity, _ in pairs(self.entityManager:getAll()) do
@@ -33,51 +52,133 @@ function World:destroy()
 	self.entityManager:destroy()
 end
 
+--[=[
+	Registers a new ComponentDefintion to the World.
+
+	When passed as a ModuleScript:
+	- Hot reloading will be enabled
+	- The ComponentDefinition will use the name of the ModuleScript if not provided.
+
+	@param componentDefinition ComponentDefinition | ModuleScript
+	@return nil
+]=]
 function World:registerComponent(componentDefinition: {} | ModuleScript)
 	self.componentRegistry:register(componentDefinition)
 end
-function World:unregisterComponent(componentDefinition: {} | ModuleScript)
-	self.componentRegistry:unregister(componentDefinition)
+
+--[=[
+	Unregisters a ComponentDefinition from the World.
+
+	@param componentResolvable ComponentResolvable 
+	@return nil
+]=]
+function World:unregisterComponent(componentResolvable: {} | ModuleScript)
+	self.componentRegistry:unregister(componentResolvable)
 end
 
+--[=[
+	Creates a new EntityQuery.
+
+	@return EntityQuery
+]=]
 function World:createQuery()
 	return EntityQuery.new(self)
 end
 
+--[=[
+	Adds a new System to the World.
+
+	When passed as a ModuleScript:
+	- Hot reloading will be enabled
+	- The SystemDefinition will use the name of the ModuleScript if not provided.
+
+	@param systemDefinition SystemDefinition | ModuleScript
+	@return nil
+]=]
 function World:addSystem(systemDefinition: {} | ModuleScript)
 	return self.systemManager:addSystem(systemDefinition)
 end
 
-function World:removeSystem(system: {} | ModuleScript)
-	return self.systemManager:removeSystem(system)
+--[=[
+	Removes a System from the World.
+
+	@param systemResolvable SystemDefinition | ModuleScript
+	@return nil
+]=]
+function World:removeSystem(systemResolvable: {} | ModuleScript)
+	return self.systemManager:removeSystem(systemResolvable)
 end
 
-function World:addComponent(componentDefinition: {}, entity: Instance | {}, data: {}?): {}
-	return self.entityManager:addComponent(self.componentRegistry:resolveOrError(componentDefinition), entity, data)
+--[=[
+	Attaches a component to an entity with given data.
+	If no data is provided, the `.default` property of the ComponentDefinition will be used.
+
+	@param componentResolvable ComponentResolvable -- the type of component to attach
+	@param entity Instance | {} -- the entity to attach the component to
+	@param data {}? -- the data to use for the component
+	@return {} -- The added component
+	@error "Already exists" -- Thrown if the given component already exists on the entity.
+]=]
+function World:addComponent(componentResolvable: {} | string, entity: Instance | {}, data: {}?): {}
+	return self.entityManager:addComponent(self.componentRegistry:resolveOrError(componentResolvable), entity, data)
 end
 
-function World:getComponent(componentDefinition: string | {}, entity: Instance | {}): {}?
-	return self.entityManager:getComponent(self.componentRegistry:resolveOrError(componentDefinition), entity)
+--[=[
+	Gets the component of the given type attached to the given entity.
+
+	Returns `nil` if no such component is attached.
+
+	@param componentResolvable ComponentResolvable -- the type of component to attach
+	@param entity Instance | {} -- the entity to get component from
+	@return {}? -- The attached component
+]=]
+function World:getComponent(componentResolvable: {} | string, entity: Instance | {}): {}?
+	return self.entityManager:getComponent(self.componentRegistry:resolveOrError(componentResolvable), entity)
 end
 
-function World:getEntitiesWith(componentDefinition: {})
-	return self.entityManager:getEntitiesWith(self.componentRegistry:resolveOrError(componentDefinition))
+--[=[
+	Gets all entities with the given component type.
+
+	@param componentResolvable ComponentResolvable -- the type of component to get entities with
+	@return {any} -- all entities in the world with the given component
+]=]
+function World:getEntitiesWith(componentResolvable: {} | string)
+	return self.entityManager:getEntitiesWith(self.componentRegistry:resolveOrError(componentResolvable))
 end
 
-function World:setComponent(componentDefinition: {}, entity: Instance | {}, data: {}): {}
-	return self.entityManager:setComponent(self.componentRegistry:resolveOrError(componentDefinition), entity, data)
+--[=[
+	Sets the data of the given component on the given entity.
+
+	@param componentResolvable ComponentResolvable -- the type of component
+	@param entity Instance | {} -- the entity with the component
+	@param data {} -- the data to set
+	@return {} -- The newly set component
+]=]
+function World:setComponent(componentResolvable: {}, entity: Instance | {}, data: {}): {}
+	return self.entityManager:setComponent(self.componentRegistry:resolveOrError(componentResolvable), entity, data)
 end
 
-function World:updateComponent(
-	componentDefinition: {},
-	entity: Instance | {},
-	data: {}
-): {}
-	return self.entityManager:updateComponent(self.componentRegistry:resolveOrError(componentDefinition), entity, data)
+--[=[
+	Updates (i.e. merges the keys) the data of the given component on the given entity.
+
+	@param componentResolvable ComponentResolvable -- the type of component
+	@param entity Instance | {} -- the entity with the component
+	@param data {} -- the data to merge into the existing data
+	@return {} -- The newly updated component
+]=]
+function World:updateComponent(componentResolvable: {}, entity: Instance | {}, data: {}): {}
+	return self.entityManager:updateComponent(self.componentRegistry:resolveOrError(componentResolvable), entity, data)
 end
 
-function World:removeComponent(componentDefinition: {}, entity: Instance | {})
-	return self.entityManager:removeComponent(self.componentRegistry:resolveOrError(componentDefinition), entity)
+--[=[
+	Removes the given component from the given entity.
+
+	@param componentResolvable ComponentResolvable -- the type of component
+	@param entity Instance | {} -- the entity with the component
+	@return nil
+]=]
+function World:removeComponent(componentResolvable: {}, entity: Instance | {})
+	self.entityManager:removeComponent(self.componentRegistry:resolveOrError(componentResolvable), entity)
 end
 
 return World
